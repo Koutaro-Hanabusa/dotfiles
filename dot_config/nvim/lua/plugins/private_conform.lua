36544ba1c393fbe07fb4c158061ce9f5f29ecf48
buri1126 <koutarouhanabusa@gmail.com>
2025-12-28 12:07:14 +0900

    Add conform.nvim for prettier/formatter support

---
+++ dot_config/nvim/lua/plugins/private_conform.lua
@@ -0,0 +1,29 @@
+return {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        javascriptreact = { "prettier" },
+        json = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        markdown = { "prettier" },
+        lua = { "stylua" },
+        python = { "black" },
+        go = { "gofmt" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+
+    -- 手動フォーマット用キーマップ
+    vim.keymap.set("n", "<leader>f", function()
+      require("conform").format({ async = true, lsp_fallback = true })
+    end, { desc = "Format buffer" })
+  end,
+}
