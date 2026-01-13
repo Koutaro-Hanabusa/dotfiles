a8ae07fdfce9bccec391a7f6fd82ec925a69aa8b
Koutaro-Hanabusa <kotaro.hanabusa@sencorp.co.jp>
2026-01-13 09:59:30 +0900

    yaziは一旦使わない

---
+++ dot_config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,30 @@
+return {
+  "mikavilpas/yazi.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    {
+      "<leader>y",
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at current file",
+    },
+    {
+      "<leader>Y",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open yazi in working directory",
+    },
+  },
+  opts = {
+    open_for_directories = true,
+    floating_window_scaling_factor = 0.9,
+    yazi_floating_window_border = "rounded",
+    keymaps = {
+      show_help = "<f1>",
+    },
+    yazi_floating_window_env = {
+      PATH = os.getenv("PATH") or "/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin",
+    },
+  },
+}
