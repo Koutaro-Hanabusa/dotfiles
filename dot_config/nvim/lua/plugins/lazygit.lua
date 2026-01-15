d24788f397b84f1b2718bccd49942dfdf31987ef
Koutaro-Hanabusa <kotaro.hanabusa@sencorp.co.jp>
2026-01-15 10:08:32 +0900

    lazygit起動時はすぐに操作できるように

--- dot_config/nvim/lua/plugins/lazygit.lua
+++ dot_config/nvim/lua/plugins/lazygit.lua
@@ -4,6 +4,14 @@ return {
     "nvim-lua/plenary.nvim",
   },
   config = function()
+    -- LazyGitが開いた時にターミナルモードで始まる
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "*lazygit*",
+      callback = function()
+        vim.cmd("startinsert")
+      end,
+    })
+
     -- キーマッピング
     vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
     vim.keymap.set("n", "<leader>gf", "<cmd>LazyGitFilter<cr>", { desc = "LazyGit current file history" })
