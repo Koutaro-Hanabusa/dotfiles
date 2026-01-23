dc1d0c5fe5210ec8271eac9e5fb878ad7eecf5f9
Koutaro-Hanabusa <kotaro.hanabusa@sencorp.co.jp>
2026-01-23 11:31:27 +0900

    js/ts用のプラグインを入れる

--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -7,6 +7,8 @@ return {
     lint.linters_by_ft = {
       markdown = { "textlint" },
       text = { "textlint" },
+      javascriptreact = { "textlint" },
+      typescriptreact = { "textlint" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
@@ -17,8 +19,22 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "<leader>l", function()
+    -- 現在のファイルをlint
+    vim.keymap.set("n", "<leader>ll", function()
       lint.try_lint()
-    end, { desc = "Trigger linting" })
+    end, { desc = "Lint current file" })
+
+    -- 現在のファイルをtextlintで自動修正
+    vim.keymap.set("n", "<leader>lf", function()
+      local file = vim.fn.expand("%:p")
+      vim.cmd("!textlint --fix " .. vim.fn.shellescape(file))
+      vim.cmd("edit!")
+    end, { desc = "Textlint fix current file" })
+
+    -- リポジトリ全体のmdファイルをチェック (quickfixに出力)
+    vim.keymap.set("n", "<leader>la", function()
+      vim.cmd('cexpr system("textlint --format unix \"**/*.md\" 2>/dev/null")')
+      vim.cmd("copen")
+    end, { desc = "Textlint all md files" })
   end,
 }
