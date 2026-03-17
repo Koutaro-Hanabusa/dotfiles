8cf8675eb84a3f9c266e97cdc284936814963f34
Koutaro-Hanabusa <kotaro.hanabusa@sencorp.co.jp>
2026-03-17 09:17:09 +0900

    fix(nvim): diffview.nvimのコンフリクト解消キーマップをactions APIに修正

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -9,6 +9,7 @@ return {
     { "<leader>dH", "<cmd>DiffviewFileHistory<CR>", desc = "File history (all)" },
   },
   config = function()
+    local actions = require("diffview.actions")
     require("diffview").setup({
       enhanced_diff_hl = true,
       view = {
@@ -19,10 +20,12 @@ return {
       },
       keymaps = {
         view = {
-          ["<leader>co"] = "<cmd>DiffviewConflictPick ours<CR>",
-          ["<leader>ct"] = "<cmd>DiffviewConflictPick theirs<CR>",
-          ["<leader>cb"] = "<cmd>DiffviewConflictPick base<CR>",
-          ["<leader>ca"] = "<cmd>DiffviewConflictPick all<CR>",
+          { "n", "<leader>co", actions.conflict_choose("ours"), { desc = "Choose OURS (left)" } },
+          { "n", "<leader>ct", actions.conflict_choose("theirs"), { desc = "Choose THEIRS (right)" } },
+          { "n", "<leader>cb", actions.conflict_choose("base"), { desc = "Choose BASE" } },
+          { "n", "<leader>ca", actions.conflict_choose("all"), { desc = "Choose ALL" } },
+          { "n", "[x", actions.prev_conflict, { desc = "Prev conflict" } },
+          { "n", "]x", actions.next_conflict, { desc = "Next conflict" } },
         },
       },
     })
